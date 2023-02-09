99c28b72958de8cc2e3db1920bf295202e8089a4
LangPiñeiro <alangp@miumg.edu.gt>
2023-02-09 12:48:26 -0600

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace AnalisisLexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(619, 588);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Escribe Código";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 18);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(601, 553);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvInfo.Location = new System.Drawing.Point(637, 132);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.RowHeadersWidth = 51;
+            this.dgvInfo.RowTemplate.Height = 24;
+            this.dgvInfo.Size = new System.Drawing.Size(692, 207);
+            this.dgvInfo.TabIndex = 1;
+            this.dgvInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInfo_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Componente léxico";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Palabra Ingresada";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Columna";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Fila";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1341, 683);
+            this.Controls.Add(this.dgvInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dgvInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AnalisisLexico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Descomponer(string palabra)
+        {
+            string caracterRerpresentado = "";
+            switch (palabra)
+            {
+                case "COMENZAR":
+                    caracterRerpresentado = "Palabra reservada de inicio";
+                    break;
+                case "STR":
+                    caracterRerpresentado = "Variable de tipo string";
+                    break;
+                case ";":
+                    caracterRerpresentado = "Signo de cierre de sentencia";
+                    break;
+                case "%":
+                    caracterRerpresentado = "Signo de asignación";
+                    break;
+                case "==":
+                    caracterRerpresentado = "Signo de comparación";
+                    break;
+                case "+":
+                    caracterRerpresentado = "Signo de suma";
+                    break;
+                case "-":
+                    caracterRerpresentado = "Signo de resta";
+                    break;
+                case ">":
+                    caracterRerpresentado = "Signo mayor";
+                    break;
+                case "<":
+                    caracterRerpresentado = "Signo menor";
+                    break;
+                case "FIN":
+                    caracterRerpresentado = "Fin del programa";
+                    break;
+                case "Fun":
+                    caracterRerpresentado = "declaración de función";
+                    break;
+                default:
+                    int val = 0;
+                    int validacion = 0;
+                    if (int.TryParse(palabra, out val))
+                    {
+                        caracterRerpresentado = "Es un número";
+                        validacion = 1;
+                    }
+                    if (palabra.StartsWith("'") && palabra.EndsWith("'"))
+                    {
+                        caracterRerpresentado = "Es un nombre de variable";
+                        validacion = 1;
+                    }
+                    if (validacion == 0)
+                    {
+                        caracterRerpresentado = "Identificador";
+                    }
+                    break;
+            }
+            return caracterRerpresentado;
+        }
+        public void Analisis()
+        {
+            string codigo = richTextBox1.Text; 
+
+            char[] jump = { '\n' };
+
+            char[] delimitador = { ' ' };
+
+            string[] lineas = codigo.Split(jump);
+
+            for (int i = 0; i < lineas.Length; i++)
+            {
+                string[] words = lineas[i].Split(delimitador);
+
+                for (int z = 0; z < words.Length; z++)
+                {
+                    words[z] = words[z].Replace("\n", "");
+
+                    if (words[z] != "")
+                    {
+                        DataGridViewRow fila = (DataGridViewRow)dgvInfo.Rows[0].Clone();
+                        fila.Cells[0].Value = Descomponer(words[z]);
+                        fila.Cells[1].Value = words[z];
+                        fila.Cells[2].Value = z + 1;
+                        fila.Cells[3].Value = i + 1;
+                        dgvInfo.Rows.Add(fila); 
+                    }
+                }
+            }
+
+        }
+
+
+       public void borrar()
+        {
+            this.dgvInfo.Rows.Clear(); 
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            borrar();
+            Analisis();
+            //personalizado(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvInfo_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
